c83233ba1bd6f593bf3940b47f7052ee1b561eb6
Chris <cr1s@hotmail.it>
2022-12-10 12:51:39 +0100

    Add: Exercise_Javadoc1

---
+++ src/Tester.java
@@ -0,0 +1,27 @@
+/**
+ * @author Chris
+ *
+ * this is the Tester class
+ */
+public class Tester {
+
+
+
+    public static void main(String[] args) {
+
+        /**
+         * Created a new Object
+         */
+        WordGames wordGames1 = new WordGames();
+
+        /**
+         * invoke addheloWord and add a word.
+         */
+        wordGames1.addHelloWord("NewUser");
+
+        /**
+         * invoke gettFullName and add the value.
+         */
+        wordGames1.gettFullName("Chris","Puglisi");
+    }
+}
---
+++ src/WordGames.java
@@ -0,0 +1,28 @@
+/**
+ * @author Chris
+ *
+ * This is the wordGames class
+ */
+
+public class WordGames {
+
+
+    /**
+     *   Create a method that return system out + String*
+     * @param word is a String
+     */
+    public void  addHelloWord(String word){
+        System.out.printf("Hello %s %n",word);
+    }
+
+    /**
+     * Create a method that return a system out + 2 String
+     * @param name is a String
+     * @param surname is a String
+     */
+    public void gettFullName(String name,String surname){
+        System.out.printf("Name: %s Surname: %s ",name,surname);
+    }
+}
+
+
